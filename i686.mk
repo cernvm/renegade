3b6b99d10a98cb6bdd8b170ab885740174023a1a
Jakob Blomer <jblomer@cern.ch>
2013-10-06 11:39:17 +0200

    add 32bit compat library build file

---
+++ i686.mk
@@ -0,0 +1,35 @@
+
+SOURCE_ROOT = $(TOP)/$(NAME)/pkg
+SOURCE_DIST = ./*
+UPSTREAM_NAME = $(shell echo $(NAME) | sed 's/-i686$$//')
+UPSTREAM_RPM_32 = $(UPSTREAM_NAME)-$(VERSION)-$(RELEASE).i686.rpm
+UPSTREAM_RPM_64 = $(UPSTREAM_NAME)-$(VERSION)-$(RELEASE).x86_64.rpm
+TREE_32 = 32-$(VERSION)-$(RELEASE)
+TREE_64 = 64-$(VERSION)-$(RELEASE)
+
+$(UPSTREAM_RPM_32): version release
+	yumdownloader $(UPSTREAM_NAME)-$(VERSION)-$(RELEASE) --archlist=i686
+	touch $(UPSTREAM_RPM_32)
+
+$(UPSTREAM_RPM_64): version release
+	yumdownloader $(UPSTREAM_NAME)-$(VERSION)-$(RELEASE) --archlist=x86_64
+	touch $(UPSTREAM_RPM_64)
+
+$(TREE_32): $(UPSTREAM_RPM_32)
+	mkdir $(TREE_32)
+	cd $(TREE_32) && cat ../$(UPSTREAM_RPM_32) | rpm2cpio | cpio -id
+
+$(TREE_64): $(UPSTREAM_RPM_64)
+	mkdir $(TREE_64)
+	cd $(TREE_64) && cat ../$(UPSTREAM_RPM_64) | rpm2cpio | cpio -id
+
+$(SOURCE_TARBALL): $(SOURCE_ROOT) version release | $(RPMTOP)/SOURCES
+	cd $(SOURCE_ROOT); \
+	  gtar -cvz --exclude=.svn --transform 's,^\.,$(NAME)-$(VERSION),' --show-transformed-names -f - $(SOURCE_DIST) > $(SOURCE_TARBALL)
+
+$(SOURCE_ROOT): $(TREE_32) $(TREE_64)
+	rm -rf $(SOURCE_ROOT)
+	mkdir $(SOURCE_ROOT)
+	cd $(TREE_32) && gtar -cv -f - `for f in $$(find . ! -type d); do if [ ! -f ../$(TREE_64)/$$f ]; then echo -n "$$f "; fi; done` | \
+	  (cd ../`basename $(SOURCE_ROOT)` && gtar -xv -f -)
+
