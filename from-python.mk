a8bcebdc9e4c872caa7cc7f0b1e2d255caa5136b
Jakob Blomer <jblomer@cern.ch>
2014-02-02 21:56:29 +0100

    proper cloud-scheduler package

--- from-python.mk
+++ from-python.mk
@@ -1,9 +1,10 @@
 
 PYTHON_SOURCE=$(NAME)-$(VERSION)
+PYTHON_INST_SCRIPT=$(shell if [ -f custom_install ]; then echo '--install-script custom_install'; fi)
 
 $(RPM): | $(PYTHON_SOURCE)
-	cd $(PYTHON_SOURCE) && python setup.py bdist_rpm
-	cp $(PYTHON_SOURCE)/dist/$(NAME)-$(VERSION)-$(RELEASE).$(ARCH).rpm $(RPM)
+	cd $(PYTHON_SOURCE) && python setup.py bdist_rpm --release $(RELEASE) $(PYTHON_INST_SCRIPT)
+	cp $(PYTHON_SOURCE)/dist/$(NAME)-$(VERSION)-$(RELEASE).$(ARCH).rpm $(RPM) 
 
 clean:
 	rm -rf $(PYTHON_SOURCE) $(RPM) 
