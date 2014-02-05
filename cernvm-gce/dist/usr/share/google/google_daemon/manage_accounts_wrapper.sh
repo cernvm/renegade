dccd47d43c47f8da47fe99cacc5b5d61179179a0
Jakob Blomer <jblomer@cern.ch>
2014-02-05 13:50:43 +0100

    bump version

--- cernvm-gce/dist/usr/share/google/google_daemon/manage_accounts_wrapper.sh
+++ cernvm-gce/dist/usr/share/google/google_daemon/manage_accounts_wrapper.sh
@@ -2,5 +2,5 @@
 
 DAEMON=/usr/share/google/google_daemon/manage_accounts.py
 
-logger "GCE: starting account manager with options $@"
+logger -- "GCE: starting account manager with options $@"
 exec $DAEMON $@
